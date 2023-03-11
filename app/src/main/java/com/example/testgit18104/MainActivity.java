f86b5de19f85e489b430a963b48fbc2e6fdf9df0
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-03-11 16:00:24 +0700

    Mở rộng tính năng

--- app/src/main/java/com/example/testgit18104/MainActivity.java
+++ app/src/main/java/com/example/testgit18104/MainActivity.java
@@ -11,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //    Thực hiện mở rộng tính năng
+    //    Thực hiện mở rộng tính năng
+    //    Thực hiện mở rộng tính năng
+    //    Thực hiện mở rộng tính năng
+    //    Thực hiện mở rộng tính năng
 }
